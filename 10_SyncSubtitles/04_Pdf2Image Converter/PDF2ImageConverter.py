bd036dd60324641bc79e5848fa1d56f260331be9
ihanseol <hanseol33@naver.com>
2023-03-10 22:44:03 +0900

    add

---
+++ 10_SyncSubtitles/04_Pdf2Image Converter/PDF2ImageConverter.py
@@ -0,0 +1,23 @@
+
+import os
+from PIL import Image
+from pdf2image import convert_from_path, convert_from_bytes
+
+def pdf_to_jpg(pdf_path):
+    # Convert each page of the PDF to a JPEG image
+    images = convert_from_path(pdf_path, dpi=300)
+    # Save each image as a JPEG file
+    for i, image in enumerate(images):
+        image.save(f"{pdf_path[:-4]}_page{i+1}.jpg", "JPEG")
+
+
+
+# Get a list of all PDF files in the current directory
+pdf_files = [f for f in os.listdir() if f.endswith('.pdf')]
+
+# Convert each PDF file to a JPEG image
+for pdf_file in pdf_files:
+    print(pdf_file)
+    pdf_to_jpg(pdf_file)
+
+
