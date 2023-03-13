069aaae8aa8798548f0ac6e2f6b94d889086b019
ihanseol <hanseol33@naver.com>
2023-03-14 00:17:58 +0900

    movie, sub select

--- 10_SyncSubtitles/sync_sub_byname.py
+++ 10_SyncSubtitles/sync_sub_byname.py
@@ -1,80 +1,91 @@
-
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 import os
+import sys
+
 
 def remove_extension(file_name):
     """Removes the extension from the given file name"""
     return os.path.splitext(file_name)[0]
 
+
 def get_extension(file_name):
     return os.path.splitext(file_name)[1]
 
 
-# cwd = os.getcwd()
-# dir_path = "/path/to/directory"  # Replace with the path to the directory you want to search in
+def main_job(mode):
+    # cwd = os.getcwd()
+    # dir_path = "/path/to/directory"  # Replace with the path to the directory you want to search in
 
-files = os.listdir()
-movie_files = []
+    files = os.listdir()
+    movie_files = [f for f in os.listdir() if f.endswith(('.mp4', '.mkv', '.avi', '.mov', '.m4v'))]
+    movie_files.sort()
 
-for file in files:
-    if file.endswith(('.mp4', '.avi', '.mov', '.mkv')):
-        movie_files.append(file)
+    if not movie_files:
+        print('folder has no movie files ...')
+        exit()
 
-movie_files.sort()
+    smi_files = [f for f in os.listdir() if f.endswith(('.smi', '.ass', '.srt', '.sub', '.sami'))]
+    smi_files.sort()
 
-if len(movie_files) == 0:
-    print('folder has no movie files ...')
-    quit()
+    file_names = []
 
+    if mode == 'mov':
+        for f in movie_files:
+            base_name = remove_extension(f)
+            file_names.append(base_name)
+    else:
+        for f in smi_files:
+            base_name = remove_extension(f)
+            file_names.append(base_name)
 
-# mp4_files = [f for f in os.listdir(cwd) if f.endswith(".mp4")]
-# mp4_files.sort()
+    file_extensions = []
 
-smi_files = [f for f in os.listdir() if f.endswith(('.smi','.ass','.srt','.sub','.sami'))]
+    if mode == 'mov':
+        for f in smi_files:
+            file_extensions.append(get_extension(f))
+    else:
+        for f in movie_files:
+            file_extensions.append(get_extension(f))
 
-smi_files.sort()
+    print('----------------------------------------------------')
+    print(f'movie files : {len(movie_files)}')
+    print(f'smi files : {len(smi_files)}')
+    print(f'filename : {len(file_names)}')
 
+    if len(movie_files) != len(smi_files):
+        print('something wrong with movie and subtitle file ....')
+        quit()
 
-file_names = []
+    # change filename by subtitle name
+    print('----------------------------------------------------')
+    i = int('0')
+    if mode == 'mov':
+        for f in smi_files:
+            old_file_name = f
+            new_file_name = file_names[i] + file_extensions[i]
+            print(new_file_name)
+            os.rename(old_file_name, new_file_name)
+            i += 1
+    else:
+        for f in movie_files:
+            old_file_name = f
+            new_file_name = file_names[i] + file_extensions[i]
+            print(new_file_name)
+            os.rename(old_file_name, new_file_name)
+            i += 1
 
-for f in movie_files:
-    base_name = remove_extension(f)
-    file_names.append(base_name)
-
-file_extensions = []
-
-for f in smi_files:
-    file_extensions.append(get_extension(f))
-
-
-print('----------------------------------------------------')
-
-print(f'movie files : {len(movie_files)}')
-print(f'smi files : {len(smi_files)}')
-print(f'filename : {len(file_names)}')
-
-
-if len(movie_files) != len(smi_files):
-    print('something wrong with movie and subtitle file ....')
-    quit()
-
-
-
-# change filename by subtitle name
-print('----------------------------------------------------')
-i = int('0')
-for f in smi_files:
-    old_file_name = f
-    new_file_name = file_names[i]+file_extensions[i]
-    print(new_file_name)
-
-    os.rename(old_file_name, new_file_name)
-    i+=1
-print('----------------------------------------------------')
-
-
-
-
+    print('----------------------------------------------------')
 
 
+if __name__ == "__main__":
+    mode = sys.argv[1]
+    if mode == 'sub':
+        print("You selected 'sub' mode.")
+    elif mode == 'mov':
+        print("You selected 'mov' mode.")
+    else:
+        print("Invalid mode. Please select either 'sub' or 'mov'.")
+        exit()
+
+    main_job(mode)
