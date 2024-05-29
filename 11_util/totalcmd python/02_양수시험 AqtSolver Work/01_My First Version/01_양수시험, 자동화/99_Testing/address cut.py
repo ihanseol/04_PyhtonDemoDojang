6122f7eea9bfc20dd561cd88b0da2808765ef173
ihanseol <hanseol33@naver.com>
2024-05-29 11:10:32 +0900

    add

--- 11_util/totalcmd python/02_양수시험 AqtSolver Work/01_My First Version/01_양수시험, 자동화/99_Testing/address cut.py
+++ 11_util/totalcmd python/02_양수시험 AqtSolver Work/01_My First Version/01_양수시험, 자동화/99_Testing/address cut.py
@@ -41,7 +41,7 @@
 input_str1 = "세종특별자치시 부강면 금병로 1263-112 번지"
 input_str2 = "충청남도 보령시 주교면 은포리 산8-1번지"
 input_str3 = "대전시 유성구 장대동 278-13번지"
-input_str4 = "전의면 유천리 227번지aaaaaaaa"
+input_str4 = "서울특별시 강남구 강남대로 382(역삼동)"
 
 
 # Function to process the input string
