060ea369db905dc510241667222d6718c00b74a6
ihanseol <hanseol33@naver.com>
2024-06-28 22:15:12 +0900

    add

--- 11_util/totalcmd python/run_aqt_project_info_setting_from YangSoo_Spec_Refactor.py
+++ 11_util/totalcmd python/run_aqt_project_info_setting_from YangSoo_Spec_Refactor.py
@@ -11,37 +11,36 @@ import pyautogui
 
 class AqtSolveProjectInfoInjector:
     def __init__(self, directory, company):
-        self.DIRECTORY = directory
-        self.COMPANY = company
-        self.DEBUG_YES = True
+        self.directory = directory
+        self.company = company
+        self.debug = True
+        self.program_path = r'C:\WHPA\AQTEver3.4(170414)\AQTW32.EXE'
+        self.delay = 0.5
+        self.is_aqt_open = False
+        self.is_block = True
         self.df = pd.read_excel(r"d:\05_Send\YanSoo_Spec.xlsx")
 
-        self.PROGRAM_PATH = r'C:\WHPA\AQTEver3.4(170414)\AQTW32.EXE'
-        self.DELAY = 0.5
-        self.ISAQTOPEN = False
-        self.IS_BLOCK = True
-
     def open_aqt(self, filename):
-        if not self.ISAQTOPEN:
-            os.startfile(self.PROGRAM_PATH)
-            self.ISAQTOPEN = True
-            time.sleep(self.DELAY)
+        if not self.is_aqt_open:
+            os.startfile(self.program_path)
+            self.is_aqt_open = True
+            time.sleep(self.delay)
 
         pyautogui.hotkey('ctrl', 'o')
         pyautogui.press('backspace')
-        pyautogui.typewrite(self.DIRECTORY + filename)
-        time.sleep(self.DELAY)
+        pyautogui.typewrite(self.directory + filename)
+        time.sleep(self.delay)
         pyautogui.press('enter')
-        time.sleep(self.DELAY)
+        time.sleep(self.delay)
 
-    def close_aqt(self, cnt=1):
-        if self.ISAQTOPEN:
-            self.ISAQTOPEN = False
+    def close_aqt(self):
+        if self.is_aqt_open:
+            self.is_aqt_open = False
 
         pyautogui.hotkey('ctrl', 's')
-        time.sleep(self.DELAY)
+        time.sleep(self.delay)
         pyautogui.hotkey('alt', 'f4')
-        time.sleep(self.DELAY)
+        time.sleep(self.delay)
 
     def main_job(self, well, address):
         def enter_project_info():
@@ -51,20 +50,17 @@ class AqtSolveProjectInfoInjector:
 
         time.sleep(0.2)
 
-        # ---- company name ---
-        pyperclip.copy(self.COMPANY)
+        pyperclip.copy(self.company)
         enter_project_info()
         pyautogui.hotkey('ctrl', 'v')
 
-        # ---- location ---
-        for i in range(3):
+        for _ in range(3):
             pyautogui.press('tab')
             time.sleep(0.2)
 
         pyperclip.copy(address)
         pyautogui.hotkey('ctrl', 'v')
 
-        # ---- well ---
         pyautogui.press('tab')
         pyperclip.copy(well)
         pyautogui.hotkey('ctrl', 'v')
@@ -74,7 +70,7 @@ class AqtSolveProjectInfoInjector:
 
         pyautogui.press('enter')
         pyautogui.hotkey('ctrl', 's')
-        time.sleep(self.DELAY)
+        time.sleep(self.delay)
 
     def get_gong_n_address(self, row_index):
         try:
@@ -109,30 +105,21 @@ class AqtSolveProjectInfoInjector:
 
     @staticmethod
     def process_address(input_str):
-        # Split the input string by spaces
         parts = input_str.split()
-
-        print(parts)  # For debugging
-
-        # Initialize the index
         i = 0
 
-        # Iterate over the parts to find the target index
         for part in parts:
             if part.endswith("면") or part.endswith("구"):
                 break
             i += 1
 
-        # Select the parts you want to keep
         result = ' '.join(parts[i:])
 
         if len(result) > 21:
             result = result.replace('번지', '')
 
         address_list = result.split()
-
         filtered_list = [item for item in address_list if not (item.endswith('아파트') or item == ',')]
-        print(filtered_list)
         address_string = ' '.join(filtered_list)
 
         return address_string
@@ -142,7 +129,7 @@ class AqtSolveProjectInfoInjector:
         return int(re.findall(r'\d+', s)[0])
 
     def get_wellno_list_insend(self):
-        os.chdir(self.DIRECTORY)
+        os.chdir(self.directory)
         aqtfiles = natsorted([f for f in os.listdir() if f.endswith('.aqt')])
 
         fn_list = []
@@ -153,22 +140,22 @@ class AqtSolveProjectInfoInjector:
 
         return fn_list
 
+    def delete_difference(self, file_list):
+        aqtfiles = natsorted([f for f in os.listdir() if f.endswith('.aqt')])
+
+        for f in file_list:
+            ffiles = fnmatch.filter(aqtfiles, f"w{f}_*.aqt")
+            for _ in ffiles:
+                os.remove(_)
+
     def process_files(self):
-        def delete_difference(file_list):
-            aqtfiles = natsorted([f for f in os.listdir() if f.endswith('.aqt')])
-
-            for f in file_list:
-                ffiles = fnmatch.filter(aqtfiles, f"w{f}_*.aqt")
-                for _ in ffiles:
-                    os.remove(_)
-
         send_list = self.get_wellno_list_insend()
         xlsx_list = self.get_gong_list()
 
         difference_set = list(set(send_list) - set(xlsx_list))
-        delete_difference(difference_set)
+        self.delete_difference(difference_set)
 
-        os.chdir(self.DIRECTORY)
+        os.chdir(self.directory)
         aqtfiles = natsorted([f for f in os.listdir() if f.endswith('.aqt')])
 
         for i in xlsx_list:
@@ -183,41 +170,37 @@ class AqtSolveProjectInfoInjector:
             wfiles = fnmatch.filter(aqtfiles, f"w{i}_*.aqt")
 
             if wfiles:
-                for j, file in enumerate(wfiles):
-                    if self.DEBUG_YES: print('Processing file: ', file)
+                for file in wfiles:
+                    if self.debug:
+                        print('Processing file: ', file)
 
                     self.open_aqt(file)
                     self.main_job(gong, address)
             self.close_aqt()
 
-        if self.DEBUG_YES: print('All files processed.')
+        if self.debug:
+            print('All files processed.')
 
 
-def ProcessYangSoo_Spec():
+def process_yangsoo_spec():
     injector = AqtSolveProjectInfoInjector("d:\\05_Send\\", "산수개발(주)")
     injector.process_files()
-    # injector.get_wellno_list()
-    # injector.get_gong_list()
 
 
 def main():
     title = 'Please choose your Company: '
-    options = ['SanSu', 'DaeWoong', 'WooKyung', 'HanIL', 'DongHae', 'HyunYoon', 'JunIL', 'BuYeo', 'TaeYang', 'SamWon',
-               'MainGeo']
-
-    MyCompany = ["산수개발(주)", "대웅엔지니어링 주식회사", "(주) 우경엔지니어링", "주식회사 한일지하수", "(주)동해엔지니어링",
-                 "(주)현윤이앤씨", "(주) 전일", "부여지하수개발 주식회사", "(주)태양이엔지", "삼원개발(주)", "마인지오 주식회사"]
+    options = ['SanSu', 'DaeWoong', 'WooKyung', 'HanIL', 'DongHae', 'HyunYoon', 'JunIL', 'BuYeo', 'TaeYang', 'SamWon', 'MainGeo']
+    my_company = ["산수개발(주)", "대웅엔지니어링 주식회사", "(주) 우경엔지니어링", "주식회사 한일지하수", "(주)동해엔지니어링", "(주)현윤이앤씨", "(주) 전일", "부여지하수개발 주식회사", "(주)태양이엔지", "삼원개발(주)", "마인지오 주식회사"]
 
     option, index = pick(options, title, indicator='==>', default_index=1)
-    company = MyCompany[index]
+    company = my_company[index]
     print(option, index, company)
 
     injector = AqtSolveProjectInfoInjector("d:\\05_Send\\", company)
-    # injector = AqtSolveProjectInfoInjector("d:\\05_Send\\", "산수개발(주)")
-
     injector.process_files()
 
 
 if __name__ == "__main__":
-    # ProcessYangSoo_Spec()
+    # process_yangsoo_spec()
     main()
+
