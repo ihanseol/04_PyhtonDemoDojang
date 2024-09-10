b9d8e77c4584f2ba2375e034163dbfeceaba87fb
ihanseol <hanseol33@naver.com>
2024-09-10 10:36:47 +0900

    add

--- 11_util/totalcmd python/02_양수시험/04_양수시험, GetTS From PDF/GetTS_FromPDF.py
+++ 11_util/totalcmd python/02_양수시험/04_양수시험, GetTS From PDF/GetTS_FromPDF.py
@@ -291,7 +291,7 @@ class AQTProcessor(AQTBASE):
                     if len(get_monitors()) == 1:
                         pyautogui.click(x=1126, y=94)  # maximize sub window 1920x1200
                     else:
-                        pyautogui.click(x=1127, y=95)  # just in case dual monitor, main FHD, sub 1920x1200 - maximize sub window 1920x1200
+                        pyautogui.click(x=1197, y=95)  # just in case dual monitor, main FHD, sub 1920x1200 - maximize sub window 1920x1200
                 else:
                     pyautogui.click(x=1127, y=95)  # maximize sub window 1920x1080
 
