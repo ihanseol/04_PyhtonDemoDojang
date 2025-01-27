4c69388203bc30174a4f2784e9871a87ca6f629e
ihanseol <hanseol33@naver.com>
2025-01-27 16:39:54 +0900

    add

--- 11_util/totalcmd python/03_지하수 이용실태 조사표/09_FireShot Numbering/PassWord.py
+++ 11_util/totalcmd python/03_지하수 이용실태 조사표/09_FireShot Numbering/PassWord.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import messagebox
 import tkinter.ttk as ttk
 import random
 import string
@@ -8,24 +7,17 @@ import pyperclip
 
 def generate_password():
     """Generates a random password based on selected options."""
-
     length = int(password_length_spinbox.get())
     characters = ""
 
     if use_uppercase_var.get():
         characters += string.ascii_uppercase
-
     if use_lowercase_var.get():
         characters += string.ascii_lowercase
-
     if use_digits_var.get():
         characters += string.digits
-
     if use_symbols_var.get():
         characters += "!#$%_()*+-/<>=?@₩^~"
-
-    if use_symbolsone_var.get():
-        characters += "!#$@!@^^&&&=%_@*=?^@"
 
     if exclude_chars_entry_var.get():
         exclude_chars_str = "1IiLl|!0Oo"
@@ -43,13 +35,8 @@ def generate_password():
         return
 
     password = ''.join(random.choice(final_chars) for i in range(length))
-
-    # Moved line to ensure password_entry is ready
     password_entry.delete(0, tk.END)
     password_entry.insert(0, password)
-    # password_entry.config(state='normal')  # Set to read-only after creation
-
-    # messagebox.showinfo("Title", password)
     return password
 
 
@@ -67,29 +54,18 @@ def generate_and_copy():
 
 
 root = tk.Tk()
-root.title(" Random Password Generator for My v1.1")
-
-# Get the screen width and height
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-
-window_width = 480
-window_height = 300
-
-# Calculate the x and y coordinates to center the window
-x = (screen_width / 2) - (window_width / 2)
-y = (screen_height / 2) - (window_height / 2)
+root.title("Sordum Random Password Generator v1.1")
 
 # Frame for password display
 password_frame = ttk.Frame(root, padding=10)
 password_frame.pack(fill=tk.X)
 
-password_label = ttk.Label(password_frame, text="Random PassWord :")
+password_label = ttk.Label(password_frame, text="Randomly generated password")
 password_label.pack(side=tk.LEFT)
 
 password_entry = ttk.Entry(password_frame, width=30)
 password_entry.pack(side=tk.LEFT, expand=True, fill=tk.X)
-password_entry.config(state='normal')  # Initially read-only
+password_entry.config(state='readonly')  # Initially read-only
 
 # copy_button_image = tk.PhotoImage(file="clipboard.png")
 # You need to create or find a clipboard icon and save it as clipboard.png in the same directory
@@ -99,7 +75,7 @@ copy_button = ttk.Button(password_frame, text="Copy", command=copy_password)  # 
 copy_button.pack(side=tk.LEFT, padx=5)
 
 # Frame for buttons
-button_frame = ttk.Frame(root, padding=5)
+button_frame = ttk.Frame(root, padding=10)
 button_frame.pack(fill=tk.X)
 
 generate_button = ttk.Button(button_frame, text="Generate Password And Copy", command=generate_and_copy)
@@ -133,16 +109,11 @@ use_symbols_check = ttk.Checkbutton(options_frame, text="Use: ! # $ % _ ( ) * + 
                                     variable=use_symbols_var)
 use_symbols_check.pack(anchor=tk.W)
 
-use_symbolsone_var = tk.BooleanVar(value=False)  # Default to False as in the image
-use_symbolsone_check = ttk.Checkbutton(options_frame, text="Use: ! # $ % _ * = ? ^ @", variable=use_symbolsone_var)
-use_symbolsone_check.pack(anchor=tk.W)
-
 not_use_frame = ttk.Frame(options_frame)  # Frame for "Not Use" to align label and entry
 not_use_frame.pack(anchor=tk.W, fill=tk.X)
 
 exclude_chars_entry_var = tk.BooleanVar(value=True)
-notuse_digits_check = ttk.Checkbutton(options_frame, text="Not Use: 1 I i L l | ! 0 O o",
-                                      variable=exclude_chars_entry_var)
+notuse_digits_check = ttk.Checkbutton(options_frame, text="Use: 1IiLl|!0Oo", variable=exclude_chars_entry_var)
 notuse_digits_check.pack(anchor=tk.W)
 
 # Frame for Password Length
@@ -156,8 +127,5 @@ password_length_spinbox = tk.Spinbox(length_frame, from_=1, to=100, width=5)  # 
 password_length_spinbox.pack(side=tk.LEFT)
 password_length_spinbox.delete(0, "end")
 password_length_spinbox.insert(0, "18")  # Default length from the image
-
-# Set the window geometry
-root.geometry(f"{window_width}x{window_height}+{int(x)}+{int(y)}")
 
 root.mainloop()
