4f82e3f030f424a5a903ea606526ff2b24b2769b
ihanseol <hanseol33@naver.com>
2025-03-19 02:27:53 +0900

    f4c2806a31533a4e49ebf4aafaada6b2

--- 11_util/totalcmd python/03_GroundWater Ussage/16_Report Maker/Merge_HWP_files.py
+++ 11_util/totalcmd python/03_GroundWater Ussage/16_Report Maker/Merge_HWP_files.py
@@ -3,25 +3,42 @@ from FileProcessing_V4_20250211 import FileBase
 import os
 
 
-hwp = Hwp()  # 한/글 실행
-fb = FileBase()
+def pagesetup(hwp):
+    my_page = {'위쪽': 20, '머리말': 10, '왼쪽': 20, '오른쪽': 20, '제본여백': 0, '꼬리말': 10, '아래쪽': 13, '제본타입': 0, '용지방향': 0,
+               '용지길이': 297, '용지폭': 210}
 
-# 작업할 디렉토리를 명시적으로 설정 (예: "C:/Users/Username/Documents")
-target_dir = "D:\\05_Send"
+    hwp.set_pagedef(my_page, "cur")
+    print(my_page)
 
-# 작업 디렉토리로 변경
-os.chdir(target_dir)
 
-# 끼워넣기
-# file_list = [i for i in os.listdir() if i.endswith(".hwpx")]
-file_list = fb.get_file_filter(".","*.hwp*")
-print(file_list)
+def merge_hwp_files():
+    hwp = Hwp()  # 한/글 실행
+    fb = FileBase()
 
-hwp.open(file_list[0])  # 첫 번째(0) 파일 열기
-for i in file_list[1:]:  # 첫 번째(0) 파일은 제외하고 두 번째(1)파일부터 아래 들여쓰기한 코드 반복
-    hwp.MoveDocEnd()  # 한/글의 문서 끝으로 이동해서
-    #  hwp.BreakPage()  # <----------------------- 페이지나누기(Ctrl-Enter)
-    hwp.insert_file(i)  # 문서끼워넣기(기본값은 섹션, 글자, 문단, 스타일 모두 유지??)
-hwp.save_as("취합본.hwp")  # 반복이 끝났으면 "취합본.hwp"로 다른이름으로저장
+    # 작업할 디렉토리를 명시적으로 설정 (예: "C:/Users/Username/Documents")
+    target_dir = "D:\\05_Send"
 
-hwp.Quit()  # 한/글 프로그램 종료
+    # 작업 디렉토리로 변경
+    os.chdir(target_dir)
+
+    # 끼워넣기
+    # file_list = [i for i in os.listdir() if i.endswith(".hwpx")]
+    file_list = fb.get_file_filter(".", "*.hwp*")
+    print(file_list)
+
+    hwp.open(file_list[0])  # 첫 번째(0) 파일 열기
+    for i in file_list[1:]:  # 첫 번째(0) 파일은 제외하고 두 번째(1)파일부터 아래 들여쓰기한 코드 반복
+        hwp.MoveDocEnd()  # 한/글의 문서 끝으로 이동해서
+        #  hwp.BreakPage()  # <----------------------- 페이지나누기(Ctrl-Enter)
+        hwp.insert_file(i)  # 문서끼워넣기(기본값은 섹션, 글자, 문단, 스타일 모두 유지??)
+
+    pagesetup(hwp)
+    hwp.save_as("MergedFile.hwp")  # 반복이 끝났으면 "취합본.hwp"로 다른이름으로저장
+    hwp.Quit()  # 한/글 프로그램 종료
+
+
+if __name__ == "__main__":
+    merge_hwp_files()
+
+
+
